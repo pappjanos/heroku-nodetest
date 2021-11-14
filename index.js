e0a878c4d5ef25a9abed05ab8414f7a44c255a83
pappjanos <papp.janos.antal@gmail.com>
2021-11-14 20:58:01 +0100

    PORT to envvar

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ app.get("/", (req, res) => {
     res.send("asdf");
 });
 
-app.listen(3333, () => console.log("server started"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log("server started"));
